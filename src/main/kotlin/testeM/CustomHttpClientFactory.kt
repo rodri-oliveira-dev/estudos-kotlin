6bd4bccc2b94d94100204eb82e220bfc1fc7b43c
Rodrigo de Oliveira <rodrigodotnet@outlook.com>
2023-03-20 08:20:53 -0300

    Update CustomHttpClientFactory.kt

--- src/main/kotlin/testeM/CustomHttpClientFactory.kt
+++ src/main/kotlin/testeM/CustomHttpClientFactory.kt
@@ -3,18 +3,23 @@ import io.micronaut.http.client.HttpClientConfiguration
 import io.micronaut.http.client.HttpClientFactory
 import io.micronaut.http.client.HttpClientOptions
 import io.micronaut.runtime.ApplicationConfiguration
+import io.micronaut.tracing.brave.instrument.http.TraceHttpClient
+import io.opentracing.Tracer
+import javax.inject.Singleton
 
 @Factory
 class CustomHttpClientFactory(
     private val applicationConfiguration: ApplicationConfiguration,
-    private val httpClientConfiguration: HttpClientConfiguration
+    private val httpClientConfiguration: HttpClientConfiguration,
+    private val tracer: Tracer
 ) {
 
     @Singleton
     fun customHttpClient(
         @Named("custom") httpClientOptions: HttpClientOptions
     ): HttpClient {
-        return HttpClientFactory.create(httpClientOptions, applicationConfiguration, httpClientConfiguration)
+        val traceHttpClient = TraceHttpClient(httpClientOptions, tracer)
+        return HttpClientFactory.create(traceHttpClient, applicationConfiguration, httpClientConfiguration)
     }
 
     @Named("custom")
